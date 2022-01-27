b38a9aa04b982f679a77e7ac860c60d70934e56e
Dinesh Bode <dineshbode@gmail.com>
2022-01-27 10:02:53 +0530

    Updated currency exchange service with database

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into currency_exchange
+(id, conversion_multiple, environment, currency_from, currency_to)
+values(1000, 75, '', 'USD', 'INR');
+insert into currency_exchange
+(id, conversion_multiple, environment, currency_from, currency_to)
+values(1001, 84, '', 'EUR', 'INR');
+insert into currency_exchange
+(id, conversion_multiple, environment, currency_from, currency_to)
+values(1002, 53, '', 'AUD', 'INR');
