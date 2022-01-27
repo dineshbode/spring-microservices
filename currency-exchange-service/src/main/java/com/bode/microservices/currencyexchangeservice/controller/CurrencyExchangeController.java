0f50defa11c157fe24e8e5fd678dda48796b17f6
Dinesh Bode <dineshbode@gmail.com>
2022-01-27 11:06:58 +0530

    Currency conversion MS calling Currency Exchange MS done

--- currency-exchange-service/src/main/java/com/bode/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/bode/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,6 +1,7 @@
 package com.bode.microservices.currencyexchangeservice.controller;
 
 import com.bode.microservices.currencyexchangeservice.CurrencyExchange;
+import com.bode.microservices.currencyexchangeservice.repository.CurrencyExchangeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,9 +16,17 @@ public class CurrencyExchangeController {
     @Autowired
     private Environment environment;
 
+    @Autowired
+    private CurrencyExchangeRepository currencyExchangeRepository;
+
     @GetMapping("currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(70));
+        CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
+
+        //Exception handling
+        if(currencyExchange == null) {
+            throw new RuntimeException("Unable to find data for "+from+" to "+to);
+        }
 
         String port = environment.getProperty("local.server.port");
         currencyExchange.setEnvironment(port);
